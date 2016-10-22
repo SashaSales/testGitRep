71c18dd41e16f33018d83f402e7a0867297a0481
SashaSales <pyskun.oleksandr@gmail.com>
2016-10-23 00:34:46 +0300

    Delete String
    Delete names

--- Main.java
+++ Main.java
@@ -6,9 +6,6 @@ class First {
 		name = name + "Kravec";
 		int i = 50;	
 		String name2 = "Petrov Ivan"
-	
-		i = i + 90
-		String newName = name + "All";
 		
 		boolean res = true;
 		boolean res2 = true;
